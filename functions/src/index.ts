6886bbec655b2ac98dfe41ddb48cde7c74acde43
Maor Magori <maor@magori.online>
2022-06-10 18:01:39 +0300

    feat: 🚧 [WIP] functions to add new user to db and inform all ali babas of the new user
    Currently unsure on how authorizers will be informed of new user.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,37 @@
 import * as functions from "firebase-functions";
+import { UserRecord } from "firebase-functions/v1/auth";
+import * as admin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = getFirestore();
+
+const usersCollection = db.collection("users");
+
+exports.onNewUserSigned = functions.auth
+  .user()
+  .onCreate(async (user: UserRecord) => {
+    await createUserRecordInFirestore(user);
+    await informAliBabasOfANewUser(user);
+  });
+
+const createUserRecordInFirestore = async (user: UserRecord) => {
+  const newUser = {
+    displayName: user.displayName,
+    email: user.email,
+    roles: { aliBaba: false, thief: false },
+  };
+
+  const userDoc = usersCollection.doc(user.uid);
+
+  await userDoc.set(newUser);
+};
+
+const informAliBabasOfANewUser = async (user: UserRecord) => {
+  const aliBabas = await usersCollection
+    .where("roles.aliBaba", "==", true)
+    .select("email")
+    .get();
+
+  // TODO: decide on a preferred informing method
+};
